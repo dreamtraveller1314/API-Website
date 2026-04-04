68b6c97ebcd4e75acac61017ab24b20224e22858
xinbinglim <xinbinglim@gmail.com>
2026-04-05 03:08:28 +0800

    api for answerbook

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-const background = document.getElementById("background");
+const btn = document.getElementById("submitBtn");
+const input = document.getElementById("questionInput");
+const answerBox = document.getElementById("answerBox");
 
-async function getBackground() {
-  const url =
-    "https://api.nasa.gov/planetary/apod?api_key=qF2xtffJ6XiUgz0UcWsXzjLHo1UZQSK41EWMegeQ";
+btn.addEventListener("click", function() {
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
+  const question = input.value.trim();
+  if (!question) {
+    answerBox.textContent = "Type first....";
+    return;
+  }
 
-        const result = await response.json();
-        console.log(result);
+  answerBox.textContent = "Consulting the book...";
+  btn.disabled = true;
 
-        if (result.media_type !== "image") {
-            console.log("APOD returned a non-image media type.");
-            return null;
-        }
-
-        return result.url;
-        } catch (error) {
-        console.log(error.message);
-        return null;
-    }
-    
-}
-
-window.onload = function () {
-  getBackground().then(function (imageUrl) {
-    if (!imageUrl) return;
-
-    console.log(imageUrl);
-
-    if (background) {
-      background.style["background-image"] = `url('${imageUrl}')`;
-    }
-  });
-};
-
-setInterval(() => {
-  // code goes here
-}, 1000); // <-- this is the interval. In this case, 1000 ms or 1 sec.
+  fetch("https://answerbook.david888.com/answers?lang=en")
+    .then(response => response.json())
+    .then(data => {
+      answerBox.textContent = data.answer;
+      btn.disabled = false;
+    })
+});
