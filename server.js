613ef4fad3a4005b4095f9ae81961a95eaf58a85
dreamtraveller1314 <xinru2509@gmail.com>
2026-04-06 00:35:02 +0800

    update

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const fetch = require("node-fetch");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://dreamtraveller1314.github.io"
+}));
 app.use(express.json());
 app.use(express.static(".")); 
 
